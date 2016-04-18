ee39de6213d1077d4211d8491bb58fe79bc8b16b
student1 <student1@jh-mbair-158-11.d214.org>
2016-04-18 10:02:20 -0500

    Created the basic outline of the sign in page and connected the needed parts to the ViewController.Swift

--- TeacherConference/ViewController.swift
+++ TeacherConference/ViewController.swift
@@ -1,4 +1,4 @@
-//
+//  Brandon Perkins
 //  ViewController.swift
 //  TeacherConference
 //
@@ -10,15 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var studentNameTextField: UITextField!
+    @IBOutlet weak var studentIdentificationTextField: UITextField!
+    @IBOutlet weak var parentNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func goToNextViewController(sender: UIButton) {
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
