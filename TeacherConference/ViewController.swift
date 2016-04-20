3472c05498e07580e1c3f9df196fecb0d2d8fef5
student1 <student1@jh-mbair-158-14.d214.org>
2016-04-20 09:35:45 -0500

    NO CHANGE

--- TeacherConference/ViewController.swift
+++ TeacherConference/ViewController.swift
@@ -10,14 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
     var information = studentInformation()
+
+    @IBOutlet weak var studentNameTextField: UITextField!
+    @IBOutlet weak var studentIdentificationTextField: UITextField!
+    @IBOutlet weak var parentNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         var studentInfo :Dictionary <String,String> = ["name": "\(information.name)", "email": "\(information.email)", "ID": "\(information.ID)", "phoneNumber": "\(information.phone)", "school": "\(information.school)"]
 
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    
+    @IBAction func goToNextViewController(sender: UIButton) {
     }
 
     
