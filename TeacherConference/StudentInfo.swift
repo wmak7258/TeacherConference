f78be1a475a78d8735eda43097c1c3537ae6af70
student1 <student1@jh-mbair-158-11.d214.org>
2016-04-21 10:59:07 -0500

    created student info class

---
+++ TeacherConference/StudentInfo.swift
@@ -0,0 +1,27 @@
+//
+//  StudentInfo.swift
+//  TeacherConference
+//
+//  Created by student1 on 4/21/16.
+//  Copyright © 2016 John Hersey High school. All rights reserved.
+//
+
+import Foundation
+
+class StudentInformation{
+    let name: String
+    let grade: Int
+    let ID: Int
+    
+    init(Name n:String, Grade g:Int, ID i:Int){
+        name = n
+        grade = g
+        ID = i
+    }
+    
+    init(){
+        name = ""
+        grade = 0
+        ID = 0
+    }
+}
