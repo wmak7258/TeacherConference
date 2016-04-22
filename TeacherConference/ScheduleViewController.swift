fbb93bf385ba84d5496932418b5beebe6a88c566
student1 <student1@jh-mbair-158-14.d214.org>
2016-04-22 09:47:48 -0500

    Created Dictionaries

--- TeacherConference/ScheduleViewController.swift
+++ TeacherConference/ScheduleViewController.swift
@@ -9,11 +9,17 @@
 import UIKit
 
 class ScheduleViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    var information = teacherInformation()
+    var information = Teacher()
+    var parentInfo = Parent()
+    var studentInfo = Student()
     @IBOutlet weak var myTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         let teacherInfo :Dictionary <String,String> = ["Teacher Name": "\(information.teacherName)", "Class": "\(information.classes)", "Room Number": "\(information.roomNumber)", "Time": "\(information.time)", "Teacher E-Mail": "\(information.teacherEmail)"]
+        let parent :Dictionary <String,String> = ["Parent Name": "\(parentInfo.parentName)", "Parent E-Mail": "\(parentInfo.parentEmail)", "Parent Phone Number": "\(parentInfo.parentPhoneNumber)"]
+        let student :Dictionary <String,String> = ["Student Name": "\(studentInfo.name)", "Student Grade": "\(studentInfo.grade)", "Student ID": "\(studentInfo.ID)"]
+        print(parent)
+        print(student)
         print(teacherInfo)
     }
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
