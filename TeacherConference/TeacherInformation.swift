e95dfab8a0ff89cb0ba01a643d388841cdcea544
student1 <student1@jh-mbair-158-18.d214.org>
2016-04-26 10:12:08 -0500

    redid teacher class - David

--- TeacherConference/TeacherInformation.swift
+++ TeacherConference/TeacherInformation.swift
@@ -3,14 +3,20 @@ import UIKit
 class Teacher {
     var teacherName: String
     var classes: String
+    var password: String
+    var school: String
+    var teacherID: String
     var roomNumber: String
     var time: String
     var teacherEmail: String
     
-    init(TeacherName n:String,Classes c:String, RoomNumber rn:String, Time t:String, TeacherEmail e:String)
+    init(TeacherName n:String,Classes c:String,Password p:String,School s:String,TeacherID i:String, RoomNumber rn:String, Time t:String, TeacherEmail e:String)
     {
         teacherName = n
         classes = c
+        password = p
+        school = s
+        teacherID = i
         roomNumber = rn
         time = t
         teacherEmail = e
@@ -20,6 +26,9 @@ class Teacher {
     {
         teacherName = ""
         classes = ""
+        password = ""
+        school = ""
+        teacherID = ""
         roomNumber = ""
         time = ""
         teacherEmail = ""
