a7270c15c33005ddb36743eb340b867aa256dfc1
student1 <student1@jh-mbair-158-14.d214.org>
2016-04-27 09:59:44 -0500

    added class class

---
+++ TeacherConference/Classes.swift
@@ -0,0 +1,42 @@
+//
+//  Classes.swift
+//  TeacherConference
+//
+//  Created by student1 on 4/27/16.
+//  Copyright © 2016 John Hersey High school. All rights reserved.
+//
+
+import Foundation
+
+class Classes {
+    var class1: String
+    var class2: String
+    var class3: String
+    var class4: String
+    var class5: String
+    var class6: String
+    var class7: String
+    var class8: String
+    init(class1 c1: String, class2 c2: String, class3 c3: String, class4 c4: String, class5 c5: String, class6 c6: String, class7 c7: String, class8 c8: String) {
+        class1 = c1
+        class2 = c2
+        class3 = c3
+        class4 = c4
+        class5 = c5
+        class6 = c6
+        class7 = c7
+        class8 = c8
+    }
+    init() {
+        class1 = ""
+        class2 = ""
+        class3 = ""
+        class4 = ""
+        class5 = ""
+        class6 = ""
+        class7 = ""
+        class8 = ""
+    }
+    
+    
+}
