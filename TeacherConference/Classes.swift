b08639a5d2d583ede8b5edb96d343fe00c3dbcda
student1 <student1@jh-mbair-158-11.d214.org>
2016-04-28 10:42:18 -0500

    working on tableview

--- TeacherConference/Classes.swift
+++ TeacherConference/Classes.swift
@@ -10,32 +10,16 @@ import Foundation
 
 class Classes {
     var class1: String
-    var class2: String
-    var class3: String
-    var class4: String
-    var class5: String
-    var class6: String
-    var class7: String
-    var class8: String
-    init(class1 c1: String, class2 c2: String, class3 c3: String, class4 c4: String, class5 c5: String, class6 c6: String, class7 c7: String, class8 c8: String) {
+    var teacher: String
+   
+    init(class1 c1: String, teacher t:String) {
         class1 = c1
-        class2 = c2
-        class3 = c3
-        class4 = c4
-        class5 = c5
-        class6 = c6
-        class7 = c7
-        class8 = c8
+        teacher = t
+       
     }
     init() {
         class1 = ""
-        class2 = ""
-        class3 = ""
-        class4 = ""
-        class5 = ""
-        class6 = ""
-        class7 = ""
-        class8 = ""
+        teacher = ""
     }
     
     
