3eadbae689dc3a47e239d24352fc16b9903bff18
student1 <student1@jh-mbair-158-08.d214.org>
2016-05-06 10:09:59 -0500

    Added Database

---
+++ TeacherConference/DataBaseController.swift
@@ -0,0 +1,21 @@
+//
+//  DataBaseController.swift
+//  TeacherConference
+//
+//  Created by student1 on 5/6/16.
+//  Copyright © 2016 John Hersey High school. All rights reserved.
+//
+
+import UIKit
+import SQLClient
+
+
+class DataBaseController: UIViewController {
+
+    var client = SQLClient()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
