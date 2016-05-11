8d3360f942c7d5da9c60ac7589590d3e654ccf01
student1 <student1@jh-mbair-158-11.d214.org>
2016-05-11 09:34:31 -0500

    gave option to send email to user

--- TeacherConference/EndViewController.swift
+++ TeacherConference/EndViewController.swift
@@ -62,11 +62,11 @@ class EndViewController: UIViewController,MFMailComposeViewControllerDelegate {
         let okAction = UIAlertAction(title: "Yes", style: .Default, handler: {
             action in
             
-            let alert = UIAlertController(title: "Email", message: "Do you want to send an email to the teacher?", preferredStyle: .Alert)
+            let alert = UIAlertController(title: "Email", message: "Do you want to send an email?", preferredStyle: .Alert)
             let cancelAction = UIAlertAction(title: "No", style: UIAlertActionStyle.Cancel, handler: {
                 action in
             })
-            let okAction = UIAlertAction(title: "Yes", style: .Default, handler: {
+            let emailTeacherAction = UIAlertAction(title: "To Teacher", style: .Default, handler: {
                 action in
                 let mailComposeViewController = self.configuredMailComposeViewController()
                 if MFMailComposeViewController.canSendMail() {
@@ -74,7 +74,16 @@ class EndViewController: UIViewController,MFMailComposeViewControllerDelegate {
                 } else {
                     self.showSendMailErrorAlert()
                 }            })
-            alert.addAction(okAction)
+            let emailMeAction = UIAlertAction(title: "To Me", style: .Default, handler: {
+                action in
+                let mailComposeViewController = self.configuredMailComposeViewController2()
+                if MFMailComposeViewController.canSendMail() {
+                    self.presentViewController(mailComposeViewController, animated: true, completion: nil)
+                } else {
+                    self.showSendMailErrorAlert()
+                }            })
+            alert.addAction(emailMeAction)
+            alert.addAction(emailTeacherAction)
             alert.addAction(cancelAction)
             self.presentViewController(alert, animated: true, completion: nil)
         })
@@ -87,11 +96,21 @@ class EndViewController: UIViewController,MFMailComposeViewControllerDelegate {
         let mailComposerVC = MFMailComposeViewController()
         mailComposerVC.mailComposeDelegate = self
         mailComposerVC.setToRecipients([teacherInfo5.teacherEmail])
-        mailComposerVC.setSubject("Sent by \(parentInfo5.parentEmail)")
+        mailComposerVC.setSubject("Conference scheduled")
         mailComposerVC.setMessageBody("\(parentInfo5.parentName) has scheduled a conference at \(timeInfo.time) for \(studentInfo5.name)'s \(classInfo.class1). For more ways to contact \(parentInfo5.parentName) his/her email is \(parentInfo5.parentEmail) and his/her phone number is \(parentInfo5.parentPhoneNumber).", isHTML: false)
         
         return mailComposerVC
     }
+    func configuredMailComposeViewController2() -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self
+        mailComposerVC.setToRecipients([parentInfo5.parentEmail])
+        mailComposerVC.setSubject("Conference scheduled")
+        mailComposerVC.setMessageBody("You, \(parentInfo5.parentName) have scheduled a conference at \(timeInfo.time) for \(studentInfo5.name)'s \(classInfo.class1) with \(teacherInfo5.teacherName). For more ways to contact \(teacherInfo5.teacherName) his/her email is \(teacherInfo5.teacherEmail).", isHTML: false)
+        
+        return mailComposerVC
+    }
+
     
     func showSendMailErrorAlert() {
         let sendMailErrorAlert = UIAlertController(title: "Could Not Send Email", message: "Your device could not send e-mail.  Please check e-mail configuration and try again.", preferredStyle: .Alert)
