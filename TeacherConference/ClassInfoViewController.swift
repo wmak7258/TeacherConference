b0b26184fbd6795dfedb7e4d7fd0cb1ca9c0e489
student1 <student1@jh-mbair-158-14.d214.org>
2016-05-20 09:56:21 -0500

    fjfjf

--- TeacherConference/ClassInfoViewController.swift
+++ TeacherConference/ClassInfoViewController.swift
@@ -25,15 +25,13 @@ class ClassInfoViewController: UIViewController {
         
         client.connect("mobileappdev.d214.org", username: "MobileAppStu", password: "M0b1l3@pp", database: "HS214PTConference") { (connect) in
             if connect {
-                self.client.execute("SELECT * FROM Teacher ") {
+                self.client.execute("SELECT * FROM Teacher") {
                     results in
-                    print(results)
                     for table in results as NSArray {
                         for row in table as! NSArray {
-                            self.teacherInfo3.teacherName = row.objectForKey("teacher_name") as! String
                             self.teacherInfo3.teacherSchool = row.objectForKey("schoolPK") as! String
                             
-                            self.teacherNameLabel.text = "Teacher's name: \(self.teacherInfo3.teacherName)"
+                            self.teacherNameLabel.text = "Teacher's name: \(self.classInfo.teacher)"
                             self.courseNameLabel.text = "Course: \(self.classInfo.class1)"
 
                             if self.teacherInfo3.teacherSchool == "008" {
