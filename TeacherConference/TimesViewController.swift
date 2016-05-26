c1ca75fa34d17cdead34b0ed49cfe294f19ab2c0
student1 <student1@jh-mbair-158-08.d214.org>
2016-05-26 16:15:16 -0500

    many change

--- TeacherConference/TimesViewController.swift
+++ TeacherConference/TimesViewController.swift
@@ -24,9 +24,10 @@ class TimesViewController: UIViewController, UITableViewDelegate, UITableViewDat
     @IBOutlet weak var myTableView: UITableView!
     
     var timesArray: [Time] = [Time]()
-    
     var hourArray: [String] = []
-    
+    override func viewWillAppear(animated: Bool) {
+        myTableView.reloadData()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -38,13 +39,10 @@ class TimesViewController: UIViewController, UITableViewDelegate, UITableViewDat
                         {
                             self.timeInformation.time = row.objectForKey("time_complete") as! String
                             print(self.timeInformation.time)
-                            self.insertTime()
                             self.id = row.objectForKey("id") as! String
-                            print(self.id)
-                            
+                            self.insertTime()
                         }
                     }
-                    
                     self.myTableView.reloadData()
                 })
             }
@@ -59,7 +57,14 @@ class TimesViewController: UIViewController, UITableViewDelegate, UITableViewDat
         }else{
             currentCell.detailTextLabel?.text = "Taken"
         }
-        
+//        if timeInformation.taken == true{
+//            let alert = UIAlertController(title: "Sorry", message: "This time slot have been taken", preferredStyle: .Alert)
+//            let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+//            alert.addAction(cancelAction)
+//            self.presentViewController(alert, animated: true, completion: nil)
+//        } else {
+//            performSegueWithIdentifier("toDone", sender: nil)
+//        }
         return currentCell
     }
     
@@ -76,25 +81,27 @@ class TimesViewController: UIViewController, UITableViewDelegate, UITableViewDat
         if segue.identifier == "toDone"{
             let NVC = segue.destinationViewController as! EndViewController
             let currentRow = myTableView.indexPathForSelectedRow?.row
+            
             NVC.studentInfo5 = studentInfo4
             NVC.parentInfo5 = parentInfo4
             NVC.teacherInfo5 = teacherInfo4
             NVC.timeInfo = timesArray[currentRow!]
             NVC.classInfo = classInfo
-            print(studentInfo4.ID)
-            client.connect("mobileappdev.d214.org", username: "MobileAppStu", password: "M0b1l3@pp", database: "HS214PTConference") { (connect) in
-                if connect {
-                    self.client.execute("update conference_schedule set studentPK = \(self.studentInfo4.ID) where id = \(self.id)", completion: { (results) in
-                        for table in results{
-                            for row in table as! NSArray
-                            {
-                                self.timeInformation.time = row.objectForKey("time_complete") as! String
-                                print(self.timeInformation.time)
-                                self.insertTime()
-                            }
+        }
+    }
+    func updateDataBase() {
+        
+        client.connect("mobileappdev.d214.org", username: "MobileAppStu", password: "M0b1l3@pp", database: "HS214PTConference") { (connect) in
+            if connect {
+                self.client.execute("update conference_schedule set studentPK = \(self.studentInfo4.ID) where id = \(self.id)", completion: { (results) in
+                    for table in results{
+                        for row in table as! NSArray
+                        {
+                            self.timeInformation.time = row.objectForKey("time_complete") as! String
+                            self.insertTime()
                         }
-                    })
-                }
+                    }
+                })
             }
         }
     }
